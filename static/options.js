e7d255fe44cbe0d2a3e33cc029f30f158763aaca
ArjunPC1121 <chandraarjun2107@gmail.com>
2024-12-05 02:54:56 +0530

    Added button rerouting in 'options' page

---
+++ static/options.js
@@ -0,0 +1,10 @@
+function courses() {
+    window.location.href = "/courses";
+}
+
+function hugging() {
+    window.location.href = "/hugging";
+}
+function scholarship(){
+    window.location.href="/scholarship"
+}
